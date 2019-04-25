1b20df9140a33b19bf621d9505f631c2081eac9b
Justin Hall <hall.justin.d@gmail.com>
2019-04-25 05:30:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
-def reverse_each_word(string)
-  i = string.length
-  while i !=0
-    puts string[i]
-    i-=1
-  end
+def reverse_each_word(sentence)
+ sentence.reverse.split.reverse.join(" ")
 end
